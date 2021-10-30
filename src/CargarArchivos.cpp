5847ac759ef222532040bfb56b7416a10da67117
lucaskruger10 <59486689+lucaskruger10@users.noreply.github.com>
2021-10-30 19:07:14 -0300

    Add files via upload

--- src/CargarArchivos.cpp
+++ src/CargarArchivos.cpp
@@ -4,28 +4,32 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-#include <pthread.h>    //lib de threads
+#include <pthread.h> //lib de threads
 
 #include "CargarArchivos.hpp"
-    
-int cargarArchivo(HashMapConcurrente &hashMap, std::string filePath) {
+
+int cargarArchivo(HashMapConcurrente &hashMap, std::string filePath)
+{
     std::fstream file;
     int cant = 0;
     std::string palabraActual;
 
     // Abro el archivo.
     file.open(filePath, file.in);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         std::cerr << "Error al abrir el archivo '" << filePath << "'" << std::endl;
         return -1;
     }
-    while (file >> palabraActual) {
+    while (file >> palabraActual)
+    {
         // Completar (Ejercicio 4)
         hashMap.incrementar(palabraActual);
         cant++;
     }
     // Cierro el archivo.
-    if (!file.eof()) {
+    if (!file.eof())
+    {
         std::cerr << "Error al leer el archivo" << std::endl;
         file.close();
         return -1;
@@ -34,45 +38,51 @@ int cargarArchivo(HashMapConcurrente &hashMap, std::string filePath) {
     return cant;
 }
 
-struct arg_struct {
+struct arg_struct
+{
     HashMapConcurrente *hashMap;
     std::vector<std::string> *filePaths;
     atomic<int> *file_index;
-}args;
+} args;
 
-void *stmf(void *varg){ // single thread multiple files
+void *stmf(void *varg)
+{ // single thread multiple files
 
     struct arg_struct *data;
-    data = (struct arg_struct*) varg;
-    
+    data = (struct arg_struct *)varg;
+
     int path_index = data->file_index->fetch_add(1); // actualizamos index
-    while((long unsigned int) path_index < (*data->filePaths).size()){
+    while ((long unsigned int)path_index < (*data->filePaths).size())
+    {
         // si estamos aca es que podemos acceder al archivo dentro de filePaths en la posicion path_index
         cargarArchivo(*data->hashMap, (*data->filePaths)[path_index]);
 
         // actualizamos index
         path_index = data->file_index->fetch_add(1);
     }
-    free(varg);
     pthread_exit(NULL); // terminar thread
 }
 
-void cargarMultiplesArchivos(HashMapConcurrente &hashMap, unsigned int cantThreads, std::vector<std::string> filePaths) {
+void cargarMultiplesArchivos(HashMapConcurrente &hashMap, unsigned int cantThreads, std::vector<std::string> filePaths)
+{
     // Completar (Ejercicio 4)
-    std::vector<pthread_t> ts (cantThreads);
-    atomic<int> count(0); 
-    for(long unsigned int i = 0; i < cantThreads; ++i){
-        std::cout << "q onda" << "q onda" << endl;
-        struct arg_struct *varg = (struct arg_struct *)malloc(sizeof(struct arg_struct));
-        varg->hashMap = &hashMap;
-        varg->filePaths = &filePaths;
-        varg->file_index = &count;
-        pthread_create(&ts[i], NULL, &stmf, (void *) varg);
+    std::vector<pthread_t> ts(cantThreads);
+    atomic<int> count(0);
+    struct arg_struct *varg = (struct arg_struct *)malloc(sizeof(struct arg_struct));
+    varg->hashMap = &hashMap;
+    varg->filePaths = &filePaths;
+    varg->file_index = &count;
+    for (long unsigned int i = 0; i < cantThreads; ++i)
+    {
+        pthread_create(&ts[i], NULL, &stmf, (void *)varg);
     }
 
-    for(unsigned int i = 0; i < cantThreads ; ++i) // joineamos threads
+    for (unsigned int i = 0; i < cantThreads; ++i)
+    { // joineamos threads
+        // std::cout << "q onda" << "q onda" << endl;
         pthread_join(ts[i], NULL);
-    
+    }
+    free(varg);
 }
 
 #endif
